8e791e46c86799572c4d0233831b7d1e21b9a6ea
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 01:16:40 -0500

    find function working

--- searches.js
+++ searches.js
@@ -13,3 +13,16 @@ function getMax() {
 	}
 	return current.data;
 }
+
+function find(data) {
+	 var current = this.root;
+	  while (current && current.data != data) {
+		   if (data < current.data) {
+			    current = current.left;
+			     }
+		    else {
+			     current = current.right;
+			      }
+		     }
+	   return current;
+}
