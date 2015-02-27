83219d089758e84e146d3c874d9c255fa96e8e83
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 01:46:07 -0500

    test code for remove that doesnt work

---
+++ testRemove.js
@@ -0,0 +1,25 @@
+//load("remove.js");
+load("occurrences.js");
+load("BSTtree.js");
+load("searches.js");
+load("traverse.js");
+load("remove.js");
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+inOrder(nums.root);
+print("\n");
+putstr("Enter a value to search for: ");
+var value = parseInt(readline());
+var found = nums.find(value);
+if (found !== null) {
+	 print("Found " + value + " in the BST.");
+}
+else {
+	 print(value + " was not found in the BST.");
+}
