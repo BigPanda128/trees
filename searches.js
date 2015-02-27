5428f0f91add2cec9f0e960ec872deaa122d6501
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 01:07:58 -0500

    getMin and getMax functions working properly

---
+++ searches.js
@@ -0,0 +1,15 @@
+function getMin() {
+	var current = this.root;
+	while (current.left !== null) {
+		current = current.left;
+	}
+	return current.data;
+}
+
+function getMax() {
+	var current = this.root;
+	while (current.right !== null) {
+		current = current.right;
+	}
+	return current.data;
+}
