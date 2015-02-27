0661aef45e876a323220e33584a32258ee493a3c
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-27 01:17:15 -0500

    find test works with only one minor change

--- testSearch.js
+++ testSearch.js
@@ -10,8 +10,27 @@ nums.insert(37);
 nums.insert(3);
 nums.insert(99);
 nums.insert(22);
+
+///////////////////////
+
 var min = nums.getMin();
 print("The minimum value of the BST is: " + min);
 print("\n");
 var max = nums.getMax();
 print("The maximum value of the BST is: " + max);
+
+/////////////////////////////
+
+inOrder(nums.root);
+print("\n");
+print("Enter a value to search for: ");
+var value = parseInt(readline());
+var found = nums.find(value);
+if (found !== null) {
+	 print("Found " + value + " in the BST.");
+}
+else {
+	 print(value + " was not found in the BST.");
+}
+
+/////////////////
